1835d779e8538d7a35572fd410ebd8273f6f7cd7
shehrozraza <shehroz.raza11@hotmail.com>
2024-05-01 22:00:11 +0500

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 function fetchImages() {
-    fetch('https://api.unsplash.com/photos/random?count=50&client_id=UIyiabS05Yr7L10K1YMVfqJBu_6-Mjpd-ACFOAdfx6k')
+    fetch('https://api.unsplash.com/photos/random?count=32&client_id=UIyiabS05Yr7L10K1YMVfqJBu_6-Mjpd-ACFOAdfx6k')
         .then(response => response.json())
         .then(data => {
-            // Clear previous images
+        console.log(data)    
             document.getElementById('imageContainer').innerHTML = '';
 
-            // Append new images
             data.forEach(imageData => {
                 const imgElement = document.createElement('img');
                 imgElement.src = imageData.urls.regular;
-                imgElement.alt = imageData.alt_description;
                 imgElement.classList.add('image');
                 document.getElementById('imageContainer').appendChild(imgElement);
             });
